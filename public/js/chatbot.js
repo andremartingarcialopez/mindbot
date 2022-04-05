22e33a33b3547045cba9212dee49660ac0f10202
maryagga <maryag0897@gmail.com>
2022-04-05 18:49:15 -0500

    Ajustes de la pagina web

---
+++ public/js/chatbot.js
@@ -0,0 +1,88 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+const BOT_MSGS = [
+  "Me he sentido triste y decaído",
+  "He tenido ganas de llorar o he llorado",
+  "Me he sentido cansado sin razón aparente",
+  "Me he sentido inquieto e intranquilo",
+  "Me he sentido mas irritable que de costumbre ",
+  "No Me he sentido útil y necesario     ",
+  "No He disfrutado mis actividades cotidianas   ",
+  "No He encontrado agradable vivir      ",
+  "No me siento atemorizado sin motivo    ",
+  "Sufro de fuertes dolores de cabeza y cuello",
+  "Siento que el corazón me late más rápido ",
+  "Orino o siento ganas de orinar con mucha frecuencia ",
+  "Sufro de dolores de estómago e indigestión ",
+  "No duermo fácilmente y descanso por las noches",
+  "Tengo pesadillas ",
+  "No Puedo conciliar el sueño en la primera media hora después de acostarme  ",
+  "No Puedo respirar cómodamente "
+];
+
+// Icons made by Freepik from www.flaticon.com
+const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Sajad";
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  botResponse();
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse() {
+  const r = random(0, BOT_MSGS.length - 1);
+  const msgText = BOT_MSGS[r];
+  const delay = msgText.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+  }, delay);
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
